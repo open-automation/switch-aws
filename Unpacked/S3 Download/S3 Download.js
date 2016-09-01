6c2a9ab31a409c07aff344da5f3d366d6bc91b62
Dominick Peluso <peluso.dominick@gmail.com>
2016-09-01 10:08:54 -0400

    Re-decrypt and upload source copy

--- Unpacked/S3 Download/S3 Download.js
+++ Unpacked/S3 Download/S3 Download.js
@@ -1,1 +1,172 @@
-NnAzevHi2/++glUtTJ9Q8MSKHwdgVTlW1cK3rCfo9esJXscRxjfJcJsPaE2821JfH0tv0FZJyQEg+KqSdSVBhlzmywSf6gMbvmdiVGvBdqMZkU4BnC62oaAAahWW0gNBwMK59s2PvL3tDElFJ2eVMe3wWqAnBvePhK5Kt6jHKzL1wHAWHUiDvTKY7hyYgzYT1DtiNiiKh1jRt4LKGD+BbSn0u1rzHOeAzhSCYfqHMX/ex+lLynaYNY4tpBZWoFL3HpzdkH9UL2rlnz5YX7i3P57hyH1GftZE8Od749LtCwzS5W5Cm40MqbmzC7oCfQOTxKwJNHmTL6laU1YV9V7ZPVC/ixcKiANUbN3n+1OcK458mlo88eGZQR8M3zzpJJZzPQlnl30lPlTTEoe6x7UOo43a8kLEa45AwbjYmoeHg6YZ7OR5MYWoL94NJecLLDW43fsjJWJYIgRA1dST15Gso2vjVe4wCGXnuex4DuFjgHksibB/Um0lNuc2nnyIpR0N68ScaZxf7GNzv3GpMUlEswZ86iJc3rsLR3+RooeBG3Ly3KGCpn+Zp7ih7dl03qoa2LwrRkaPj8Q3Dv+spQYHJmwLmePlIDwOp/8SO8OkeOhctdiVieIttwqj8SrVetVvsW09eIAEQ8+Q97MmI7ypeha58KIbPeShKT8s9r/Ff5PmgxWBusnrJjUl00lKEdMGx5dxUNyLeLf9XG6zwzf9qDzJSSoSIvTCMtDZLxWz+sZWtuQvdMkfa4vzNLSBbdequlbSv16/rfRPFlxHX4EIirsrzqE5UpPKdGNDtwR36tPlIHi9er+EgnSwu9WyPbC9NWb87fnW/xl9YF1wb1NDNo4+llvhuirob8njdAWjMI9+kyryEP7yG3QngI178JwI0wks6g5LrtpWFTqXp4pN+dLq/oGCS8PKm6ajlIv3s/sGQjtfvrLQnZnu9XWRYCAjUjNAfTBfFvqhrv9y6uSgK5Qmib97BBdDxk8pP2aa3E/FIBOq0QSpsIZewIgUb9xposFzKM1R28/rbQNwK6ER25hrbVCAzSLlXE8igRPdeivcKImjgMAKh5kwWgRtmt95qCgJ/8QfYze/890TSRuCQueEVnu+7xYQALj/WXMHqTpo2dwFYrXZQ2m8X8i9+MdBtXyyX+FNeFmWmDr7oHv075Y2xfp1Vkij2ENdecJvSx93RKRwB2Ec5eXJgg0oIMsZcawsnCXam2Ctaw1RmCHVLlnnvqinPg5czqXxyitbPHygrSrwgruDAwsV7JWVR4X6Es/GzOCHzT13XwjOanJl9jptw4SK4fhtdL0hJwez2W9XVGjJlvZ7gmjDsqPCaIyJPrKd33fJdU8KFgYga4fgc0TB0+IqhYggR5LIoy9OxTXwozAcfwvbEif3FZKbPld5lpztg8H61tOp7Qs6OWvj117O+lxsg+qT6kPXp/0Jcx6pJmFmKrqVvYiP3AWEmB3Tef5qF6TJdDFArYOcpEmNn09p91LLG3+90UnLz0FmFTTlnWERjJLCEIoMAjmOF8dTYHA6O/HIUswCtqHvzBP47OV5y3HMxJ8VIMCD1YkRrCmLCJWRMjjs4L4JsH87MZf1x/z8kWjGcTOb9R+U3YhxFoumoo099oEGS8sDUwR5Tkum66T6kvgFVGNugIOd6pSiUovRwSF3fvQHaTCLAOL21uid241PRPTNKi4zScOM6pvP0Qhhwv85CovQAe13s8PM3QyiUGRiwQPPeEn1AMH2IXWOSxkK6IK8mJKKutFkny7HvSm5u3glzkeTduUrSlyqX3o7f6ad/KIyd1j0W+wI/3O3ES7NvYjsp9H7HLLK3g/mHY+YjC4+LcEyFlOlWGGQAQW8td2EN0Ltc614cNAp+mvX848wOyccc9ajN6nowPi7YPbUclq0m2He8stU+xehuI3UAfFiqknNvuAFnCBXaLxUtn+ecyOzXvo1zjqIRkYksV2hoYaQp8riuAwyQzjWQ/pYbJNiG3KaT37DCJKg6gBPuoWtgYcNy6CeJvnD0JA054bGeiTfBCDHSzxShvQYR4X7sDulvYrWjK+mFxahXlg4H70smXNXs3R61yjXp6o4csoYKqNtVVKobazl8UFCx2ZbCARlcAe2aDtbx8x8Uht6MchXBJfpMQXFywR+4bd+7xZEuVXnnOG1XFSb9DknlrJMDRYmeIeec8yfhitC4NIkqOtLJL5CYKzNTvNDIVA//cYpJWCZL5uTcpNWE4vIsvRsjgRPbNiZy6J57Oh0JMtt6xW4iVe5c53H33qHfZDI8ghAkcrWVDgZaUYc81IlAwpVondGG3YKlaH/89gpsYuKdZLH7wDoa2g48hrlqwrvXcJDHJhKGN5U0Nh6GBFddV/dQzgIsx7BhsifR6YWK5DNiOI1MawQyZHANi+he4D7XZDXrng7iGLsikDF4Onaqw6Wi20P+AKcwQi6AVcFrqVkyWTckZCkgUzQwS1pQq/c3MqWRkmf084SUIepCXIqOYNZeF+/kJ8MXS4PcdsdP5UPoZQkmESQ+ICqEgsaTRFjgUcKqErp+y74waQ4cM24ahtkA10Aw8Ze+MTV8uNzIMJLiVs9CQRo1Vjy3YYHi1WlJ047fxv3fp8H7bD/ITJKhDhNDmMicti9lZrPOo6Xk6HZsf8ccoekXOCFz5Z8oscISx8r4EHl0NjJ3fBr/ezEOLCOYYxzHW6UMdaz327iK+ithluUHa4fTphN3lf1Qel9Q+6HGvKIP6wYa/if0MieuGMz0FisqDK00zv2cD4x1b4gvAayDsIhhbc76tdvjdDfc87yVN/c+tcN7FJNebAz5VHBPxtggT6ZIv7FJFd+ZnUpNbSKKWPhWLTlaqJ9dKCeoudYlVwbAQ34jrQ40dbI+Y/oOKc90ktdI+9HoPuVX3kKLGeKbPsiz1gKXBYOO0E2G5w5uiAeDhDSHKncCWndWbDuTrQkgFf5a5XMjtvbwYzXdSkkEZQiXOTkJ6oIfZc1qP1kOPZwJ0y+Pl7P5wdxCm1M7vaP1yACXJz0jetTisX4j1j0jW4C7F+junVOY0yQ/QiSi7yvVZYdspkFGS+IeOuvD6i90ShM09rSfYpc1VNW/ZBYmIzTDpIJ+8OZ3hBJn4W3EMa9fNqcT22JL76eX7SjzlLZysYqISnKc5KkprDfZlxMwrfXBlaPydgaT77YV+ytfr1IWOIrr3IteFK85kgLYzujdvSIzU5lsg4Kc+3Z9yV9xk+QmrX7hl6mFICZ3BFGZT4U7eEzxV/2k1NQFmhvgarJ/iI+uaeO9+KrTS9wH4JjVpiw1zmDgn8LcxXhum7CJsmQIcNpYjdOBZ7l/iMJU0HLcU0ugmSQdMBfu+0baM/MRXeTZc024Po2TLlJlEvE8HEiWLnaTanKLtZ+KseDiNhiF3MV13Psvu2Suwpl1U4GajNxywD/gBIijYGwENEDuQ0HtENRrgJtvlFIhcwV/6h2mbnLtq9TLVTreAtqfiTTfb5D9SQNsj6CdbS0d1n2Y234txeAUo93UYta9wNG+BuPFzLiA/EoxRLAZv01c975+dBhhmnu585syW0d+0NvEqt2L3oYJBkCbNF4CDCD3feXEidFpf7TYfStxlcZE5A48srxNEMMeiRf5sPcPSHbO8//VHGYBuwAhYaj2oEfCpoa1kYqJTDReyc4Nu4b6ROV5dGHeGJevdrZw/yTnUarGXVoC7eKgEtNCtXvn++87B657I5+geSHRdKU8hWXNFBxzkLSdOwMTJ6CFFTBWcOa6QiRvTR1p5GgTw7CVnZgmQmMF84beiPkNqnV60JrCgUjHGYxALjcLfj6YD0qXzUbHNFYBBMp/cTOGXc0lDpy6tC2pqFI7ayVQFpxjkmfnNS4C243FsC3lW62kowlVg45zrYSL9l+xpAhuI1NQXsuXKwQeJ3eC2aCgO6VMWpVDoiqbKTj4usvDVD77kX2ld+pfFlHURTR3ETRkn7Ijmz5EnfiBSGZ4BpTzYxVG8hhh00rqCI6DZ1oP0sdfx/k4Vl6T1+OeHDAvun/t4Lge3dGu0tA7lQJDcfhhYuvkH95k8qGeJb2pTDB6b3JXKeTYp+Z0BSpIH0TDvpNPKMVpXVt0Cv/ElvvykYUQK+AIS8iX33tRCDMgcLtQHcd+yk0CfCmxNN+9RwbTZ7XlhNcZPVR2v0i0rmIEVFX52+XoDCvIogtdt9r7rCFBCNgUuXxO3ycrtvuv1SLGuODtpR7nAFVwVBEAE7Ed/0djO2gSkviFvt9ttKXxhkZUaZZIxQY4a19deWheAoPEF14JuGHtkUSci7xSQHoWaLAebnEqS0VzpYsNRE4asNxUhTKaaranYj4qteb8Nypr5ek5fgzJmx9gmkYVceR9HY+dq/VtxxS09AsGTtXQCpRVqbtYxSwqeTqdYs3iuByhTBeHgVSqFOc+OPQfkfmNYy/80tLvp6X6I68paEyZLxfE+9MaNFZ9sackiHOcsVU58Y/KLh9znggecoyaPDJNDvnm6i4K1jE/NfZaZ1hAxeHtNaBnLVUwFTiwA8Eg2/Mswivh253Mp3vM2jOaOgZLWfVGhthUXklOXEBU/Gu00+4ZdrTqaxCoemF+6LfKhmzwDG+yqjNFiofV5FzC0nNmW5pZtNlQ04+FEDtIJ/sx/O7Mi+tBPpiFB34NwDUzO/klDl/ulM8oo/84wEvT0nvUZfnvYKdxKSpPjbdUbtoZflZcQrxqRFttoWm5R1NmRfn8ZRf2sLa2cWm355D25Qt3DH7RuCeJxBUXqVgIWeeF/1haC1SSkaB9g4Aajo/hdsJM+iE6aCbgeIF/uzYpm+7aTwqpwL9LcopCjBhcn5IiMjxVPtxNgMqyHOyawFdKoH/BnZ3khu2SU5nAUArkZmG5zW1K+FWIJs++L002VoLwRkngl8X4b8Am3awb1KZYl0AcUL/t6S9TpXRbdKyNhDhL7RkyXQFQPzVg03T0DuumlfBLsVtFTzK8UthlbCyykdCh7S6TW4a9gdfQsdpenEQFyrAl4qI7IpHxQ2IEYRx1m638fyDGZ7cvMKDEgM3cglCrnrKGHJHSGuwIu3uZE4NYiV/O0KysWXSBTJzDdBk9JuZ2EvoZxkJq9cWRp5Z/rVLD45XCPoREwq+OGAiMFKHl8kTcZ7T6ZQd/O4Hs1Xx/JHBvXcI7/1JUm/ycWIfl7IIIdKYpZPcVP5uG2EdtoioAEXVHWpdDkxubrCw3tbixayHD3FXnTj6FlLFRL6ladSEUGIuE1Hw+fEkMepasbkriy6C5xRdcB/4J6WwvVEtJRRmKfZjUM8hOg+dPXnl1vqRZW2Rp3otLZUxLMutEKVJ8Hol1l9H6lIe6/SyVWkFpWDuoG7FnqeDIJJRIJvFUN2S68a1Dg0e7Qj8Ks8TKpmsiu8UtsgMmOBkC1vRAMKwF+kA9SBs0Q8N371s8XwQGA1G6UWdbgNP1rekGPd6PTK2ehKL0mQwJSO96U9/EDClqncZX2qIu32pAHk+RWUdDsiaG+wWZyqW4yVBUKN2QeiYFzzDJqyjh5OUD/9KQzXNQJOOGTt+NgnZmIqOiTuO8HGhgqjPwN1ZLBBqkT8wvp3FLkqf6mbudzcUqVCINZjyrASfa9yoDKitcWCED4xAk7TKoOkCNegGoGZx7XczpKCtrU1lfScyHYZWcvyXN7AOGELZ9pk6A1tWLBrnkpYUG5A/74dIS9QXSW264o0F7I2skcTrtdJb/7fgT9f/twql5wDXdGg+BSphj741AT8SncCJ1YrsoPhEUfb5FozjjeKFrINUIiIckHcmFpQ6RtSIuZsVshwdfob3JJWchvUFxWUXxa24WVRXHdw2qhYuxnMeSKDl/dIcFmt01V9OaCUTdZDX+0MvEq3+ImXG0zzPtfgigZNIWMC9TYbfJJSqjyzI6qAU7maEa7GoMoQJxE9k7LXLoMtZmmCjMC3rAjKuSFeQAc62VPbIx4+GbKIe/nWplNRg3aTasyJsJzYUKKuvpAiRPGg1MgAu4QdjnsFlRPHVduqdPd1KqHTdxYtprrXzeKf6kZdNJAELj9Id0wkZdpu5GjPRQbBzwj2IKKA9ehwkFsL+HdizEuG+ZCedLpLADfDDk81uVz6WePWWUU4om20TZZM188tO4O+BtONEb7C+dZkJfuVgnLLaa4cxHeXGygBOjkb6pxSCv3SyKXf1W/csh/EES0IUTEMC+CL+gkyFoqj+3i2W+XMfIOCD5b806nGqE90zsyGVWGTamUkIX5/Z+rbcJQmmiytXTWHi6sG8A6hQghlTExraiAg4ePNnO+yvHynetXQISpEMABOfV8Q7sH6eXKZ7IxnyAgZA8Ux38MHs/q+bizOpXfLqy2IUAkpw3LLQfFk74rNOIHULnwSzC3Jdedc6f5eDntFp3qDg+aD6+y58VQco5kCj9LHB1xydXVvizdUmOXAovXd43pi2jixoQhmoDSQ3gGQ8tn+Q8nfghwLW+V1Kl5mcFJfEJv4x1zjY06TVNbmjw9gGHvuTTpgaTfM3Em7yHnzdoIACBRmxjhHVO2xVdHBjE99oB8rW7hAUUrmPUlpL75phHHcNTnN1CaHshfaOFlQBXeQbK6hEeL0HgykC6Lmp2P+S/dRHHqT2mGbLaDdX1YO9fBINSNmQkqUwgj0a55JhxPuEIrSL3r4S69IWLx8c+wrY43/LvhXD5A4LwMqSUrYglwIjzG11VT/s1P4hfOXm+jjQn0nFpnHi4Ug+MKcPjw4QnA+elXxMTW6zi8I81q9lnWQciLbHiOn9iFzHYGGp4iu4ahr33naIUNEhDSF+m+EppTUwEaUTW64uDR37+5/Lwhe2/8imBpYhDy6jOs5fD2fh8XLE8FJ8WXH11K9FizH2R9qY6exAGkla/TKFRrPeYazc1whWNdPaMbPPoVOyzBV3WnVvmdMjEX0tK1Bln9OHM/A4wjbkgRMMgQBdO68WxIXN6dPF++gjCLXSrlvwTDADe81+n7AHnIEsnvyskCo3qCMKo76uiEnoxk1Of8gi1p4cCqHv5h3PspwmV2pNduC+tEuzbayzxhynFNbt375oSXhwXVrf2JcWpgoEjUIeboJMvn7Azw4VD4pLl3DqrL0o4HqVflYowofeaNDP5rJ0/q2YvjVsU496WgS9lyIbHAPi17dg9Y8vPTZ0TDoK4WiuQCwq2/wgJQ1zFsGe2ckKa3Kix6HsaBNBMrlDzqgZglVd8B4ck/zg7qqZHXUN38q+lvARY2dJRmD8VNipSMFIyGBEtGsZWME/SYf+oNsDfXiTZRwjOdBI9xhny9/LtJ03g11cuiyVpnzpgjaYtmzXsRbvpjrwARsQ0rpqnvGTL59G3RooUZL9qAq3Xy/Q+wqFzsUuMhj0aaYE0MfsPROI28zJrHMmQTEGTHGUgslrozDUfTlyHfw2dVo5coe4dTqvEBDYdgsR0ZOZ02TmtxK/hW4QrQ2Lwg==
+function timerFired( s : Switch )
+{
+	// Get flow element properties
+	var intervalSeconds = s.getPropertyValue("checkIntervalMinutes")*60;
+	var targetBucket = s.getPropertyValue("targetBucket");
+	var leaveOriginals = s.getPropertyValue("leaveOriginals");
+	var region = s.getPropertyValue('region');
+	var namedProfile = s.getPropertyValue('namedProfile');
+	var prefix = s.getPropertyValue('prefix');
+	var CliPathPrefix = s.getPropertyValue('CliPathPrefix');
+
+	var debug = s.getPropertyValue("debug");
+
+	// Set the log level
+	var logLevel = 2;
+
+	// Set the timerInterval on start
+	if (s.getTimerInterval() == 0){
+        s.setTimerInterval(intervalSeconds);
+	}
+
+	// Log some stuff
+	if(debug == "Yes"){
+		s.log(logLevel, "Debug output");
+		s.log(logLevel, "intervalSeconds: "+intervalSeconds);
+		s.log(logLevel, "targetBucket: "+targetBucket);
+		s.log(logLevel, "leaveOriginals: "+leaveOriginals);
+	}
+
+	// Function for adding optional params
+	var addOptionalParameters = function(cmd, restrictTo){
+		if(region) 			cmd += " --region "+region;
+		if(namedProfile) 		cmd += " --profile "+namedProfile;
+		if(prefix && restrictTo == 'list-objects'){
+								cmd += " --prefix "+prefix;
+		}
+		// Booleans
+		return cmd;
+	}
+
+	// Function for explicitly calling Python
+	var addCliPathPrefix = function(cmd, CliPathPrefix)
+	{
+		fixedCmd = CliPathPrefix + cmd;
+		return fixedCmd;
+	}
+
+	// Function to see if AWS CLI is installed
+	var verifyAwsCli = function()
+	{
+		cmd = addCliPathPrefix("aws --version", CliPathPrefix);
+		Process.execute(cmd);
+		var awsVersionError = Process.stderr;
+		var awsVersionResponse = Process.stdout;
+		if(debug == 'Yes'){
+			s.log(logLevel, "aws version response: "+awsVersionResponse);
+			s.log(logLevel, "aws version error: "+awsVersionError);
+		}
+		if(!awsVersionError){
+			s.log(3, "AWS CLI does not appear to be installed!");
+			s.log(3, "You may have to set (or unset) the CLI Path Prefix element property so Switch may execute 'aws' commands.");
+		} else {
+			return true;
+		}
+	}
+
+	// Function to see if an S3 bucket exists and is accessible
+	var verifyS3Bucket = function(bucketName)
+	{
+		Process.execute(addCliPathPrefix("aws s3api head-bucket --bucket "+bucketName, CliPathPrefix));
+		var awsHeadBucketResponse = Process.stderr;
+		if(awsHeadBucketResponse){
+			s.log(3, awsHeadBucketResponse);
+			return false;
+		} else {
+			if(debug == 'Yes') s.log(logLevel, "Bucket '"+bucketName+"' exists and is available to you.");
+			return true;
+		}
+	}
+
+	// Function to list all objects in an S3 bucket
+	var listS3Objects = function(bucketName)
+	{
+		// Get list of S3 objects
+		list_cmd = addOptionalParameters(addCliPathPrefix("aws s3api list-objects --bucket "+targetBucket+" --output json", CliPathPrefix), 'list-objects');
+		if(debug == "Yes") s.log(2, 'list_cmd: '+list_cmd);
+		Process.execute(list_cmd);
+		var listObjectsResponse = Process.stdout;
+		// Evaluate response
+		var parsedObject = eval("(" + listObjectsResponse + ")");
+		var objects = parsedObject.Contents;
+		// Return
+		return objects;
+	}
+
+	// Function to get objects
+	var getS3Objects = function(inputObjects)
+	{
+		var i = null;
+		for(i = 0; i < inputObjects.length; i++){
+			key = inputObjects[i].Key;
+			size = inputObjects[i].Size;
+			basename_key = key.replace(/^.*[\\\/]/, '');
+
+			// Check for empty object AWS sometimes returns to maintain the "folder" structure on the S3 console
+			if(key == prefix || size === 0){
+				if(debug == "Yes"){ s.log(logLevel, "Empty 'folder' object found and ignored: "+key) };
+				continue;
+			};
+
+			// Log some stuff
+			if(debug == "Yes"){
+				s.log(logLevel, "Key: "+key);
+				s.log(logLevel, "Basename key: "+basename_key);
+			}
+
+			// Create a new job container
+			job = s.createNewJob(targetBucket+"_"+key);
+			fn = job.createPathWithName(basename_key, false);
+
+			// Invoke AWS CLI
+			download_cmd = addOptionalParameters(addCliPathPrefix("aws s3api get-object --bucket "+targetBucket+" --key \""+key+"\" \""+fn+"\" --output json", CliPathPrefix), 'No');
+			if(debug == "Yes") s.log(2, 'download_cmd: '+download_cmd);
+			Process.execute(download_cmd);
+			downloadError = Process.stderr;
+
+			// Evaluate response
+			if(downloadError){
+				s.log(3, "AWS CLI S3 download error: "+downloadError);
+			} else {
+				// Log some stuff
+				if(debug == "Yes") s.log(logLevel, "Object successfully downloaded : "+key);
+				// Complete job
+            	job.sendToSingle(fn);
+				// Delete object
+				if(leaveOriginals == 'No'){
+					// Invoke
+					delete_cmd = addOptionalParameters(addCliPathPrefix("aws s3api delete-object --bucket "+targetBucket+" --key \""+key+"\" --output json", CliPathPrefix), 'No');
+					if(debug == "Yes") s.log(2, 'delete_cmd: '+delete_cmd);
+					Process.execute(delete_cmd);
+					deleteError = Process.stderr;
+					deleteResponse = Process.stdout;
+					// Log some stuff
+					if(debug == "Yes") s.log(logLevel, "deleteResponse : "+deleteResponse);
+					// Check for errors
+					if(deleteError){
+						s.log(3, "AWS CLI S3 delete error: "+deleteError);
+					} else {
+						if(debug == "Yes") s.log(logLevel, "File successfully removed from S3.");
+					}
+				}
+			}
+		}
+	}
+
+	// Ensure AWS CLI is installed
+	if(verifyAwsCli()){
+
+		// Verify S3 bucket
+		if(verifyS3Bucket(targetBucket)){
+			// List objects
+			var objects = listS3Objects(targetBucket);
+			// Check to see if any objects exist
+			if(objects.length > 0){
+				// Log some stuff
+				if(debug == "Yes")s.log(logLevel, "num of objects found: "+ objects.length);
+				// Get all objects
+				getS3Objects(objects);
+			}
+		}
+	}
+} 
